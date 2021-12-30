1ed0682c397edd8a5fad15b358065b4394272bcf
Eren Kara <eren.kara@hepsiburada.com>
2021-12-30 12:35:14 +0300

    chapter 2 solutions

---
+++ Chapter 2/chessBoard.js
@@ -0,0 +1,33 @@
+/* 
+Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines.
+At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
+Passing this string to console.log should show something like this:
+
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # #
+When you have a program that generates this pattern,
+define a binding size = 8 and change the program so that it works for any size,
+ outputting a grid of the given width and height. */
+
+
+ const size = 8;
+ let board = "";
+
+ for( let i = 0; i < size; i++ ){
+    for(let j = 0; j < size; j++){
+        if( (i+j) % 2 === 0){
+            board += ' '
+        } else {
+            board += '#'
+        }
+    }
+    board += '\n'
+ }
+
+ console.log(board)
---
+++ Chapter 2/looping_a_triangle.js
@@ -0,0 +1,16 @@
+/*Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+####### 
+
+*/
+
+
+for(let block = '#'; block.length <=7; block += '#'){
+    console.log(block)
+}
