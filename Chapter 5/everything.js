e56eab1c280fe5e7f0d9ef2f9491e6805e92c05a
Eren Kara <eren.kara@hepsiburada.com>
2022-01-03 20:37:04 +0300

    chapter 5 solutions

---
+++ Chapter 5/everything.js
@@ -0,0 +1,20 @@
+/*
+Analogous to the some method, arrays also have an every method.
+This one returns true when the given function returns true for every element in the array.
+In a way, some is a version of the || operator that acts on arrays, and every is like the && operator.
+Implement every as a function that takes an array and a predicate function as parameters.
+ Write two versions, one using a loop and one using the some method.
+*/
+
+function everyWithLoop(array, test){
+    for (element of array){
+            if (!test(element)){
+                return false
+            }
+        }
+    return true
+}
+
+function everyWithSome(array, test){
+    return !array.some((element) => !test(element))
+}
